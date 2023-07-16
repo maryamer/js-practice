3309aade6fb6dfc0a82b3fd2dc168ff69c75a35a
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-16 21:08:54 +0430

    built-in constructors of js"

---
+++ builtInConstructorFunction.js
@@ -0,0 +1,19 @@
+const strLit = "maryam";
+const strObj = new String("maryam");
+
+console.log(typeof strLit, typeof strObj);
+
+const numLit = 2;
+const numObj = new Number(2);
+
+console.log(typeof numLit, typeof numObj);
+
+const arrayLit = [2];
+const arrayObj = new Array([2]);
+
+console.log(typeof arrayLit, typeof arrayObj);
+
+const boolLit = true;
+const boolObj = new Boolean(true);
+
+console.log(typeof boolLit, typeof boolObj);
