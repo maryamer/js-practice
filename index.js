e8e6d0eeb8187c96c09cd9b59490cf3bad425aac
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-16 19:59:58 +0430

    create discount function

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+const discount = (price) => {
+  const discountPerPrice = 10;
+  const minPrice = 100;
+  const discountSteps = 3;
+  const maxDiscount = 25;
+  let discount =
+    Math.floor((price - minPrice) / discountPerPrice) * discountSteps;
+  if (price < minPrice + discountPerPrice) {
+    return console.log("no discount");
+  }
+  if (discount > maxDiscount) {
+    return console.log(maxDiscount);
+  }
+  console.log(discount);
+};
+discount(100);
