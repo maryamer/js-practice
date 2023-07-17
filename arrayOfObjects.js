50930489c8c52f510782840ab67da30d81dfeca2
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-17 23:58:05 +0430

    what type of data to select

---
+++ arrayOfObjects.js
@@ -0,0 +1,19 @@
+// what type of data to select
+
+// when list data => array,set
+// array => order
+// set => unique
+
+// when data object or pack => object, map
+//map => key :primitive and object , iterable data
+
+// // // // array of objects // // // //
+
+// json =>{name,email,role}
+// json =>[{},{},{}]
+
+const users = [
+  { name: "maryam", age: 28 },
+  { name: "ali", age: 18 },
+  { name: "mohammad", age: 22 },
+];
