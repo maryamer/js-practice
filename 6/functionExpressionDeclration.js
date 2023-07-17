2025e603cba63e09d23b13c8edf4342c8fd9cdb0
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-18 00:44:06 +0430

    arrow functions

--- 6/functionExpressionDeclration.js
+++ 6/functionExpressionDeclration.js
@@ -8,7 +8,7 @@ function add1(a, b) {
 }
 add1(1, 2);
 
-// function Expression // //
+// //function Expression // //
 // we can not call function before itself in function expression
 // handlers in component => expression
 
@@ -18,3 +18,17 @@ const add = function (a, b) {
   return a + b;
 };
 add(1, 2);
+
+// // // arrow function ()=>{}  === function expression // // //
+
+// function calculateDiscount(disc, price) {
+//   const total = price * (1 - disc / 100);
+//   console.log(total);
+// }
+
+const calculateDiscount = (disc, price) => {
+  return price * (1 - disc / 100);
+};
+const calculateDiscount2 = (disc, price) => price * (1 - disc / 100);
+
+calculateDiscount(5, 100);
