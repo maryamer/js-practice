3b3cfffb46fbdd658777e25a40d6635a2ae66571
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-21 00:31:22 +0430

    array basic methods

---
+++ 66.arrayBasicMethods.js
@@ -0,0 +1,10 @@
+const arr = ["A", "B", "C", "D"];
+
+// 1. length
+// 2. push :add element to end of array and returns the new length of the array
+// 3. pop : removes the last element of array and returns it
+// 4. shift : Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+// 5. unshift : Inserts new elements at the start of an array, and returns the new length of the array.
+// 6. indexOf : Returns the index of the first occurrence of a value in an array, or -1 if it is not present.
+// 7.includes: Determines whether an array includes a certain element, returning true or false as appropriate.
+console.log(arr.includes("E"));
