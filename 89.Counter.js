ef62ac68531525ca816187822b3d4b26cd2143a1
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-24 21:18:20 +0430

    counter project section2

--- 89.Counter.js
+++ 89.Counter.js
@@ -1,9 +1,9 @@
 // counter project section1
-const incBtn = document.querySelector(".increment");
-const decBtn = document.querySelector(".decrement");
-const resetBtn = document.querySelector(".reset");
-const counterValue = document.querySelector(".counter_value");
-let count = Number(counterValue.textContent);
+// const incBtn = document.querySelector(".increment");
+// const decBtn = document.querySelector(".decrement");
+// const resetBtn = document.querySelector(".reset");
+// const counterValue = document.querySelector(".counter_value");
+// let count = Number(counterValue.textContent);
 // const inc = () => {
 //   count++;
 //   counterValue.textContent = count;
@@ -16,15 +16,36 @@ let count = Number(counterValue.textContent);
 //   count = 0;
 //   counterValue.textContent = count;
 // };
+// const counter = (e) => {
+//   const classList = e.target.classList;
+//   if (classList.contains("reset")) count = 0;
+//   else if (classList.contains("increment")) count++;
+//   else count--;
 
-const counter = (e) => {
-  const classList = e.target.classList;
-  if (classList.contains("reset")) count = 0;
-  else if (classList.contains("increment")) count++;
-  else count--;
+//   counterValue.textContent = count;
+// };
+// incBtn.addEventListener("click", counter);
+// decBtn.addEventListener("click", counter);
+// resetBtn.addEventListener("click", counter);
 
-  counterValue.textContent = count;
-};
-incBtn.addEventListener("click", counter);
-decBtn.addEventListener("click", counter);
-resetBtn.addEventListener("click", counter);
+//
+//
+//
+//
+
+// counter project section2
+const counterValue = document.querySelector(".counter_value");
+let count = Number(counterValue.textContent);
+const btns = document.querySelectorAll(".btn");
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const classList = e.target.classList;
+    if (classList.contains("reset")) count = 0;
+    else if (classList.contains("increment")) count++;
+    else count--;
+    if (count > 0) counterValue.style.color = "green";
+    else if (count === 0) counterValue.style.color = "gray";
+    else counterValue.style.color = "red";
+    counterValue.textContent = count;
+  });
+});
