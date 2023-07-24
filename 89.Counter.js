df6b1e279a79797625f77094c98ec3ae23cb376d
maryameb <personalmaryamebrahimi@gmail.com>
2023-07-24 21:56:26 +0430

    modal mini project

--- 89.Counter.js
+++ 89.Counter.js
@@ -34,18 +34,42 @@
 //
 
 // counter project section2
-const counterValue = document.querySelector(".counter_value");
-let count = Number(counterValue.textContent);
-const btns = document.querySelectorAll(".btn");
-btns.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    const classList = e.target.classList;
-    if (classList.contains("reset")) count = 0;
-    else if (classList.contains("increment")) count++;
-    else count--;
-    if (count > 0) counterValue.style.color = "green";
-    else if (count === 0) counterValue.style.color = "gray";
-    else counterValue.style.color = "red";
-    counterValue.textContent = count;
-  });
+// const counterValue = document.querySelector(".counter_value");
+// let count = Number(counterValue.textContent);
+// const btns = document.querySelectorAll(".btn");
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", (e) => {
+//     const classList = e.target.classList;
+//     if (classList.contains("reset")) count = 0;
+//     else if (classList.contains("increment")) count++;
+//     else count--;
+//     if (count > 0) counterValue.style.color = "green";
+//     else if (count === 0) counterValue.style.color = "gray";
+//     else counterValue.style.color = "red";
+//     counterValue.textContent = count;
+//   });
+// });
+
+//
+//
+//
+//
+
+// // modal mini project
+
+const showModalBtn = document.querySelector(".show-modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+showModalBtn.addEventListener("click", (e) => {
+  // backdrop, modal => remove hidden class
+  backdrop.classList.remove("hidden");
+  modal.classList.remove("hidden");
 });
+function closeModal() {
+  // backdrop, modal => add hidden class
+  backdrop.classList.add("hidden");
+  modal.classList.add("hidden");
+}
+closeModalBtn.addEventListener("click", closeModal);
+backdrop.addEventListener("click", closeModal);
