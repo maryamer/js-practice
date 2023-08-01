89a207f1091078aad4582561f1e437f069e29e2f
maryameb <personalmaryamebrahimi@gmail.com>
2023-08-01 12:08:52 +0430

    callback hell

--- Async javascript/app.js
+++ Async javascript/app.js
@@ -31,9 +31,9 @@
 //
 
 // callback=> call back later
-// 1. login user
-// 2. enrolled courses
-// 3. episode
+// 1. login user => email
+// 2. email => enrolled courses
+// 3. course title=> episode
 
 // callback
 // promise
@@ -45,7 +45,26 @@ function loginUser(email, pass, callback) {
     callback({ userEmail: email, userPass: pass });
   }, 2000);
 }
+function enrolledCouses(email, callback) {
+  setTimeout(() => {
+    callback(["react", "node", "next"]);
+  }, 1500);
+}
+function getEpisode(courseTitle, callback) {
+  setTimeout(() => {
+    callback(["E01", "E02", "E03"]);
+  }, 1000);
+}
+
 loginUser("myEMAIL", "mypassword", (userData) => {
   console.log(userData);
+  enrolledCouses(userData.email, (courses) => {
+    console.log(courses);
+    getEpisode(courses[0], (episodes) => {
+      console.log(episodes);
+    });
+  });
 });
 [].map(() => {});
+
+// callback hell
