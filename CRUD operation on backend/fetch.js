d7cd590f1cd2ac427e49e715dde5f39813780aef
maryameb <personalmaryamebrahimi@gmail.com>
2023-08-03 00:24:38 +0430

    error handling while using fetch method

--- CRUD operation on backend/fetch.js
+++ CRUD operation on backend/fetch.js
@@ -24,19 +24,53 @@ function getUsers() {
     .catch();
 }
 
-function loginUser() {
-  const user = {
-    email: "eve.holt@reqres.in",
-    password: "cityslicka",
-  };
-  fetch("https://reqres.in/api/login", {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => res)
-    .then((res) => console.log(res))
-    .catch();
+// function loginUser() {
+//   const user = {
+//     email: "eve.holt@reqres.in",
+//     // password: "cityslicka",s
+//   };
+//   fetch("https://reqres.in/api/login", {
+//     method: "POST",
+//     body: JSON.stringify(user),
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((res) => {
+//       if (res.ok) {
+//         return res;
+//       } else {
+//         return res.error;
+//       }
+//     })
+//     .then((res) => console.log(res))
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// }
+
+// error handling
+
+async function loginUser() {
+  try {
+    const user = {
+      email: "eve.holt@reqres.in",
+      // password: "cityslicka",
+    };
+    const res = await fetch("https://reqres.in/api/login", {
+      method: "POST",
+      body: JSON.stringify(user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await res.json();
+    if (result.ok) {
+      return result;
+    }
+    throw new Error(result.error);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
