cd012939980408dbe8c3653bcef72d4e1105e8f1
maryameb <personalmaryamebrahimi@gmail.com>
2023-08-04 00:36:27 +0430

    render filtered products on DOM

--- searchFilter/index.js
+++ searchFilter/index.js
@@ -1,4 +1,6 @@
 const searchInput = document.querySelector("#search");
+const productList = document.querySelector(".products");
+const btns = document.querySelectorAll(".btn");
 let allProductsData = [];
 const filters = {
   searchItems: "",
@@ -9,18 +11,48 @@ document.addEventListener("DOMContentLoaded", () => {
     .then((res) => {
       allProductsData = res.data;
       //   render products on DOM
+      renderProducts(allProductsData, filters);
     })
     .catch((err) => console.log(err));
 });
 function renderProducts(_products, _filters) {
   const filteredProducts = _products.filter((p) => {
-    console.log(_filters.searchItems);
     return p.title.toLowerCase().includes(_filters.searchItems.toLowerCase());
   });
-  console.log(filteredProducts);
+  productList.innerHTML = ``;
+  //   render to dom
+  filteredProducts.forEach((item) => {
+    // create
+    // content
+    // append
+    let productsDiv = document.createElement("div");
+    productsDiv.classList.add("products");
+    let product = `
+    <div class="img-container">
+      <img src=${item.img} alt="" />
+    </div>
+    <div class="product-desc">
+      <p class="product-price">${item.price}</p>
+      <p class="product-">${item.title}</p>
+    </div>
+  </div>`;
+    productsDiv.innerHTML = product;
+    productList.appendChild(productsDiv);
+  });
 }
 
 searchInput.addEventListener("input", (e) => {
   filters.searchItems = e.target.value;
   renderProducts(allProductsData, filters);
+});
+// render filtered products on DOM
+
+// filter based on groups
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const filter = e.target.dataset.filter;
+    filters.searchItems = filter;
+    console.log(filters.searchItems);
+    renderProducts(allProductsData, filters);
+  });
 });
