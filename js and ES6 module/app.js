12ebd8b8ab62adc9d0d6bf498388df6be4f061df
maryameb <personalmaryamebrahimi@gmail.com>
2023-08-06 00:24:59 +0330

    module exports method -2

---
+++ js and ES6 module/app.js
@@ -0,0 +1,4 @@
+const utils = require("./utils");
+console.log(utils());
+// returns exports object
+// console.log(utils.fetchApi());
---
+++ js and ES6 module/utils.js
@@ -0,0 +1,15 @@
+const userName = "maryam";
+
+function fetchApi() {
+  console.log("fetch");
+}
+// module.exports.userName = userName;
+// module.exports.fetchApi = fetchApi;
+
+// module.exports={
+//   userName,
+//   fetchApi
+// }
+
+module.exports = fetchApi;
+console.log(module);
